65ccd62d90006ec0511260a256f2038fe1108232
JodsintZ <jodsint@gmail.com>
2019-10-02 19:04:27 +0700

    too easy

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    printf("Hello World\n");
+    //cd
+    //g++ file_name.cpp && ./a.exe (Windows)
+    //g++ file_name.cpp && ./a.out (Linux)
+
+    return 0;
+}
