d375eed1e344805be6f3ea7d478f3c57bd70d085
JodsintZ <jodsint@gmail.com>
2020-05-02 21:47:32 +0700

    2 ez

--- TOI/toi11_place.cpp
+++ TOI/toi11_place.cpp
@@ -1,28 +1,38 @@
 #include <bits/stdc++.h>
 
 #define long long long
-#define pii pair<int,int>
 #define tii tuple<long,int,int>
-#define x first
-#define y second
 
 using namespace std;
 
-int n, m;
+const int N = 2e5+5;
+const int M = 1e6+5;
+
+int n, m, par[N];
 priority_queue<tii> pq;
+long ans;
+
+int find(int x) { return par[x] = x == par[x] ? x : find(par[x]); }
 
 int main() {
     scanf("%d %d", &n, &m);
-    for(int i = 0, b, c, long c; i < m; i++) {
+    for(int i = 0; i < N; i++) par[i] = i;
+    for(int i = 0, b, a; i < m; i++) {
+        long c;
         scanf("%d %d %lld", &a, &b, &c);
-        pq.emplace_back(c, a, b);
+        pq.emplace(c-1, a, b);
     }
     while(!pq.empty()) {
-        tii now = pq.top();
+        tii now = pq.top(); 
         pq.pop();
-        long c = get<2>(now);
-
+        long c = get<0>(now);
+        int a = get<1>(now), b = get<2>(now);
+        int pa = find(a), pb = find(b);
+        if(pa != pb) {
+            par[pa] = pb;
+            ans += c;
+        }
     }
-
+    printf("%lld ", ans);
     return 0;
 }
